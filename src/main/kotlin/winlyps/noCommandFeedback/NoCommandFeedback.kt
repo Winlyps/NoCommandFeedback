ab3df4e3e050a8d1c4f601e8d2d9bfdfb7a6b8e9
Winlypse <nts3@o2.pl>
2024-10-22 23:32:12 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noCommandFeedback/NoCommandFeedback.kt
@@ -0,0 +1,14 @@
+package winlyps.noCommandFeedback
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoCommandFeedback : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
