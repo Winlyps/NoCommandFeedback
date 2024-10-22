edfba4602950b29c26cdbc57fdda7333fd7ac3b5
Winlypse <nts3@o2.pl>
2024-10-22 23:32:21 +0200

    Changes!

--- src/main/kotlin/winlyps/noCommandFeedback/NoCommandFeedback.kt
+++ src/main/kotlin/winlyps/noCommandFeedback/NoCommandFeedback.kt
@@ -6,9 +6,14 @@ class NoCommandFeedback : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("sendCommandFeedback", "false")
+        }
+        logger.info("NoCommandFeedback plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoCommandFeedback plugin has been disabled.")
     }
 }
